23bfd0f3a6b50b88fbd654e9bc46c897879b1fae
Othi <mnpq.raven@gmail.com>
2025-04-16 13:08:48 +0700

    feat(jis): 1st iteration using jis

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::{fs, path::Path};
+use encoding_rs::SHIFT_JIS;
+use std::{error::Error, fs, path::Path};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let file_path = Path::new("./sample/1.cue");
     let maybe_content = fs::read(file_path);
-    if let Ok(content) = maybe_content {
-        let maybe = String::from_utf8(content.clone());
-        println!("utf8 validation: {:?}", maybe);
-
-        for (index, bytecode) in content.iter().enumerate() {
-            let c = char::from(*bytecode);
-            if (index >= 84) {
-                print!("{}", c);
-            }
-        }
+    if maybe_content.is_err() {
+        println!("maybe_content err'd");
+        return Ok(());
     }
+
+    let bytes = maybe_content.unwrap();
+
+    let (result, _encoding_used, had_errors) = SHIFT_JIS.decode(&bytes);
+    println!("{}", result);
+    assert!(!had_errors);
+    Ok(())
 }
