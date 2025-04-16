ea492b9ed2a3b14b4b3fc8e046e1223ee6aa8c38
Othi <mnpq.raven@gmail.com>
2025-04-16 13:52:48 +0700

    dev

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
-use encoding_rs::SHIFT_JIS;
-use std::{error::Error, fs, path::Path};
+use encoding_rs::{EUC_JP, Encoding, GBK, ISO_2022_JP, SHIFT_JIS};
+use std::{error::Error, fs, io, path::Path};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file_path = Path::new("./sample/1.cue");
-    let maybe_content = fs::read(file_path);
-    if maybe_content.is_err() {
-        println!("maybe_content err'd");
-        return Ok(());
+    let decoders: [&'static Encoding; 4] = [SHIFT_JIS, ISO_2022_JP, EUC_JP, GBK];
+
+    for decoder in decoders.iter() {
+        let dir = Path::new("./sample").read_dir().unwrap();
+        for dir_entry in dir.into_iter() {
+            let path = dir_entry.unwrap().path();
+            if let Ok(result) = try_decode(path.clone(), decoder) {
+                println!(
+                    "DECODING FILE {} WITH {} SUCCESSFUL!",
+                    path.file_name().unwrap().to_string_lossy(),
+                    decoder.name()
+                );
+                println!("{}", result);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn try_decode<P: AsRef<Path>>(
+    path: P,
+    encoding: &'static Encoding,
+) -> Result<String, std::io::Error> {
+    let maybe_content = fs::read(path);
+    if let Err(_e) = maybe_content {
+        panic!("FILE NOT FOUND");
     }
 
     let bytes = maybe_content.unwrap();
 
-    let (result, _encoding_used, had_errors) = SHIFT_JIS.decode(&bytes);
-    println!("{}", result);
-    assert!(!had_errors);
-    Ok(())
+    let (result, _encoding_used, had_errors) = encoding.decode(&bytes);
+    match had_errors {
+        true => Err(std::io::Error::from(io::ErrorKind::InvalidData)),
+        false => Ok(result.into_owned()),
+    }
 }
